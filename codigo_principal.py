2d8507b4663dc04816252820593d9cd3e09c9f1d
Gabriel "Stromoth" Costa <gabrielpaulocosta2020@gmail.com>
2025-08-23 11:48:20 -0300

    Update codigo_principal.py
    Novas funcionalidades adicionadas e mudanças na "interface" dos menus de escolha

--- codigo_principal.py
+++ codigo_principal.py
@@ -1,28 +1,46 @@
 import os
-from classes import Mercado, Produto
+from classes import Mercado, Vendedor
 
 os.system('cls')
 loja = Mercado('EMC Produtos')
+vendedor = Vendedor('Gabriel')
 
 while True:
     print(f'Sistema da {loja.nome}')
-    print('1 - Cadastrar Produto')
-    print('2 - Ver Estoque')
-    print('3 - Sair')
+    print('1 - Gerenciar estoque')
+    print('2 - Vender')
+    print('3 - Ver saldo')
+    print('4 - Sair')
     escolha = int(input('-> '))
+
     os.system('cls')
     match escolha:
-        case 1:
-            print('CADASTRAR PRODUTO')
-            nome = input('Nome: ')
-            preco = input('Preço: ')
-            quantidade = int(input('Quantidade: '))
-            produto = Produto(nome, preco, quantidade)
-            loja.cadastrar_produto(produto)
-            os.system('cls')
-        case 2:
-            loja.mostrar_estoque()
-            input('Digite 1 para voltar\n-> ')
-            os.system('cls')
-        case 3:
+        case 1: # Gerenciar estoque
+            while True:
+                print(f'Sistema da {loja.nome}')
+                print('1 - Cadastrar produto')
+                print('2 - Remover produto')
+                print('3 - Editar produto')
+                print('4 - Ver estoque')
+                print('5 - Voltar')
+
+                escolha = int(input('-> '))
+                os.system('cls')
+                match escolha:
+                    case 1:
+                        loja.cadastrar_produto()
+                    case 2:
+                        loja.remover_produto()
+                    case 3:
+                        loja.editar_quantidade(loja)
+                    case 4:
+                        loja.mostrar_estoque()
+                    case 5:
+                        os.system('cls')
+                        break
+        case 2: # Vender
+            vendedor.vender(loja)
+        case 3: # Ver saldo
+            vendedor.ver_saldo()
+        case 4: # Sair
             break
