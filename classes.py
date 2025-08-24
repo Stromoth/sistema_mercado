94c35a70bb097aefc4c74f5351a2f86e197e8623
Gabriel "Stromoth" Costa <gabrielpaulocosta2020@gmail.com>
2025-08-24 02:02:48 -0300

    Update classes.py
    Melhorias no funcionamento dos métodos;
    Método para organizar a forma como o estoque lista os produtos com base em diferentes critérios;
    Início de um decorator para fazer um arquivo que registra as vendas realizadas no sistema.

--- classes.py
+++ classes.py
@@ -1,4 +1,34 @@
 import json, os
+from itertools import count
+from functools import wraps
+
+'''def log_vendas(func):
+    @wraps(func)
+    def wrapper(produto, quantidade):
+
+        return wrapper'''
+
+def gerar_id():
+    if os.path.exists("id.json"):
+        with open("id.json", "r") as arquivo:
+            ultimo_id = json.load(arquivo)
+    else:
+        ultimo_id = 0 
+
+    contador = count(ultimo_id + 1)
+
+    novo_id = next(contador)
+
+    with open("id.json", "w") as arquivo:
+        json.dump(novo_id, arquivo)
+
+    return novo_id
+
+def encontrar_produto(estoque, id_procurado):
+    for produto in estoque:
+        if produto["ID"] == id_procurado:
+            return produto
+    return None
 
 class Produto:
     def __init__(self, nome, valor, quantidade):
@@ -8,18 +38,29 @@ class Produto:
     
     def editar_quantidade(self, loja):
         print('EDITAR QUANTIDADE')
-        id = int(input('Insira o ID do produto\n-> ')) - 1
+        id = int(input('Insira o ID do produto\n-> '))
+        produto_encontrado = encontrar_produto(loja.estoque, id)
+        os.system('cls')
+
+        print(f'Editando quantidade do produto {produto_encontrado["Nome"]}')
         print('1 - Aumentar quantidade\n2 - Diminuir quantidade')
         escolha = int(input('-> '))
+        os.system('cls')
+
         match escolha:
             case 1:
+                print(f'Editando quantidade do produto {produto_encontrado["Nome"]}')
                 quantidade = int(input('Quantidade a ser aumentada\n-> '))
-                loja.estoque[id]['Quantidade'] += quantidade
+                produto_encontrado['Quantidade'] += quantidade
             case 2:
+                print(f'Editando quantidade do produto {produto_encontrado["Nome"]}')
                 quantidade = int(input('Quantidade a ser diminuida\n-> '))
-                loja.estoque[id]['Quantidade'] -= quantidade
+                produto_encontrado['Quantidade'] -= quantidade
         with open('estoque.json', 'w', encoding='utf-8') as arquivo:
             json.dump(loja.estoque, arquivo, ensure_ascii=False, indent=2)
+        
+        input('Digite 1 para continuar\n-> ')
+        os.system('cls')
 
 class Mercado(Produto):
     def __init__(self, nome):
@@ -38,33 +79,44 @@ class Mercado(Produto):
             quantidade = int(input('Quantidade: '))
 
             produto = Produto(nome, preco, quantidade)
+            
+            dict_produto = {
+                'ID':gerar_id(),
+                'Nome':produto.Nome,
+                'Preço':produto.Valor,
+                'Quantidade':produto.Quantidade
+            }
 
-            produto = vars(produto)
-            self.estoque.append(produto)
+            self.estoque.append(dict_produto)
 
             with open ('estoque.json', 'w', encoding='utf-8') as arquivo:
                 json.dump(self.estoque, arquivo, ensure_ascii=False, indent=2)
-                print(f'Produto {produto["Nome"]} cadastrado com sucesso')
+                print(f'Produto {dict_produto["Nome"]} cadastrado com sucesso')
 
             input('Digite 1 para voltar\n-> ')
             os.system('cls')
     
     def remover_produto(self):
         print('REMOVER PRODUTO')
-        id = int(input('Insira o ID do produto que quer remover\n-> ')) - 1
+
+        id = int(input('Insira o ID do produto que quer remover\n-> '))
+        produto_encontrado = encontrar_produto(self.estoque, id)
 
         with open ('estoque.json', 'r', encoding='utf-8') as arquivo:
             self.estoque = json.load(arquivo)
+        os.system('cls')
 
-        print(f'O produto {self.estoque[id]["Nome"]} será removido')
+        print(f'O produto {produto_encontrado["Nome"]} será removido')
         print('Continuar?')
         print('1 - Sim\n2 - Não')
 
         escolha = int(input('-> '))
+        os.system('cls')
+
         match escolha:
             case 1:
-                print(f'O produto {self.estoque[id]["Nome"]} foi removido do sistema')
-                del self.estoque[id]
+                print(f'O produto {produto_encontrado["Nome"]} foi removido do sistema')
+                self.estoque.remove(produto_encontrado)
                 with open ('estoque.json', 'w', encoding='utf-8') as arquivo:
                     json.dump(self.estoque, arquivo, ensure_ascii=False, indent=2)
                 input('Digite 1 para continuar\n-> ')
@@ -79,14 +131,16 @@ class Mercado(Produto):
         print(f'='*31)
         print(f'Estoque da {self.nome}')
         print(f'='*31)
-
-        with open ('estoque.json', 'r', encoding='utf-8') as arquivo:
-            self.estoque = json.load(arquivo)
+        if os.path.exists('estoque.json'):
+            with open ('estoque.json', 'r', encoding='utf-8') as arquivo:
+                self.estoque = json.load(arquivo)
+        else:
+            with open ('estoque.json', 'a', encoding='utf-8') as arquivo:
+                json.dump(' ', arquivo)
 
         for i in self.estoque:
-            print(f'ID: {self.estoque.index(i) + 1}')
             for k, v in i.items():
-                if k == 'Valor':
+                if k == 'Preço':
                     print(f'{k}: R${v:.2f}')
                 else:
                     print(f'{k}: {v}')
@@ -94,41 +148,83 @@ class Mercado(Produto):
             print(f'='*31)
         input('Digite 1 para voltar\n-> ')
         os.system('cls')
+    
+    def reordenar_estoque(self):
+        print(f'='*31)
+        print(f'Reordenar estoque')
+        print(f'='*31)
 
+        print('1 - Ordenar por nome(A-Z)')
+        print('2 - Ordenar por preço(Menor-Maior)')
+        print('3 - Ordenar por quantidade em estoque(Mais-Menos)')
+        print('4 - Ordenar por ID(Menor-Maior)')
+
+        escolha = int(input('-> '))
+        os.system('cls')
+
+        match escolha:
+            case 1:
+                self.estoque = sorted(self.estoque, key=lambda x: x['Nome'])
+                print('Estoque ordenado em ordem alfabética.')
+            case 2:
+                self.estoque = sorted(self.estoque, key=lambda x: x['Preço'])
+                print('Estoque ordenado com base nos preços.')
+            case 3:
+                self.estoque = sorted(self.estoque, key=lambda x: x['Quantidade'], reverse=True)
+                print('Estoque ordenado com base nas quantidades.')
+            case 4: 
+                self.estoque = sorted(self.estoque, key=lambda x: x['ID'])
+                print('Estoque ordenado com base nos IDs.')
+        with open('estoque.json', 'w', encoding='utf-8') as arquivo:
+            json.dump(self.estoque, arquivo, ensure_ascii=False, indent=2)
+        print(f'='*31)
+
+        input('Digite 1 para continuar\n-> ')
+        os.system('cls')
+        
 class Vendedor:
     def __init__(self, nome):
         self.Nome = nome
-        with open('vendedor.json', 'r', encoding='utf-8') as vendedor:
-            dados_do_usuario = json.load(vendedor)
-            self.Saldo = dados_do_usuario['Saldo']
+        try:
+            with open('vendedor.json', 'r', encoding='utf-8') as vendedor:
+                dados_do_usuario = json.load(vendedor)
+                self.Saldo = dados_do_usuario['Saldo']
+        except:
+            self.Saldo = 0
 
         with open('vendedor.json', 'w', encoding='utf-8') as vendedor:
             json.dump(vars(self), vendedor, ensure_ascii=False, indent=2)
 
+    '''@log_vendas'''
     def vender(self, loja):
-        id = int(input('Insira o ID do produto vendido\n-> ')) - 1 
-        quantidade = int(input(f'\nInsira a quantidade vendida do produto {loja.estoque[id]["Nome"]}\n-> '))
+        id = int(input('Insira o ID do produto vendido\n-> '))
+        produto_encontrado = encontrar_produto(loja.estoque, id)
+        os.system('cls')
 
-        print(f'O produto vendido foi {loja.estoque[id]["Nome"]}')
-        print('Confimar venda?\n1 - Confirmar\n2 - Cancelar')
+        quantidade = int(input(f'Insira a quantidade vendida do produto {produto_encontrado["Nome"]}\n-> '))
+        os.system('cls')
 
+        print(f'CONFIRMAR VENDA\nProduto: {produto_encontrado["Nome"]}\nQuantidade: {quantidade}')
+        print('\n1 - Confirmar\n2 - Cancelar')
         escolha = int(input('-> '))
+        os.system('cls')
+
         match escolha:
             case 1:
-                if loja.estoque[id]['Quantidade'] - quantidade < 0:
+                if produto_encontrado['Quantidade'] - quantidade < 0:
                     print('Não foi possível realizar a venda.')
                     print('Quantidade de produtos insuficiente no estoque.')
                 else: 
-                    print(f'O vendedor {self.Nome} vendeu {quantidade} unidade(s) do produto {loja.estoque[id]["Nome"]}.')
+                    print(f'O vendedor {self.Nome} vendeu {quantidade} unidade(s) do produto {produto_encontrado["Nome"]}.')
 
-                    valor_recebido = ( loja.estoque[id]["Valor"] ) * quantidade
+                    valor_recebido = ( produto_encontrado["Preço"] ) * quantidade
                     print(f'Recebeu R${valor_recebido:.2f}.')
 
                     self.Saldo += valor_recebido
                     with open('vendedor.json', 'w', encoding='utf-8') as vendedor:
                         json.dump(vars(self), vendedor, ensure_ascii=False, indent=2)
 
-                    loja.estoque[id]['Quantidade'] -= quantidade
+                    produto_encontrado['Quantidade'] -= quantidade
                     with open('estoque.json', 'w', encoding='utf-8') as arquivo:
                         json.dump(loja.estoque, arquivo, ensure_ascii=False, indent=2)
 
@@ -136,6 +232,7 @@ class Vendedor:
             case 2:
                 input('Digite 1 para continuar\n-> ')
         os.system('cls')
+        '''return produto_encontrado['Nome'], quantidade'''
     
     def ver_saldo(self):
         with open('vendedor.json', 'r', encoding='utf-8') as vendedor:
